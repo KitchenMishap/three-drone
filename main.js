bda03e06205f0436daab780d63e80e982ba13b18
NigelGSharpUK <130097204+KitchenMishap@users.noreply.github.com>
2024-06-23 02:33:53 +0100

    Bumpmap for ground

--- main.js
+++ main.js
@@ -6,24 +6,30 @@ import { Sky } from 'three/addons/objects/Sky.js';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 3;
-camera.position.y = -10;
+camera.position.z = 30;
+camera.position.y = -100;
 camera.quaternion.setFromAxisAngle(new THREE.Vector3(1,0,0), Math.PI/2);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 // Green Floor
+const texture = new THREE.TextureLoader().load( "textures/chess.jpg" );
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.repeat.set( 1, 1 );
 const planeGeom = new THREE.PlaneGeometry( 1000, 1000 );
-const planeMat = new THREE.MeshLambertMaterial( {color: 0x00ff00, side: THREE.FrontSide} );
+const planeMat = new THREE.MeshLambertMaterial( {color: 0x88ff88, side: THREE.FrontSide} );
+planeMat.bumpMap = texture;
 const plane = new THREE.Mesh( planeGeom, planeMat );
+
 scene.add( plane );
 
 // Cube
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const geometry = new THREE.BoxGeometry( 20, 20, 20 );
 const material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
 const cube = new THREE.Mesh( geometry, material );
-cube.position.z = 2
+cube.position.z = 20
 scene.add( cube );
 
 // Sky
