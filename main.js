71773bff417727ba7da4de3ffda07669467ad787
NigelGSharpUK <130097204+KitchenMishap@users.noreply.github.com>
2024-06-23 01:20:28 +0100

    Lambertian surface and multicoloured lighting

--- main.js
+++ main.js
@@ -10,9 +10,24 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
+
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+// Red directional light at half intensity shining from the top.
+var directionalLightRed = new THREE.DirectionalLight( 0xff0000, 0.5 );
+directionalLightRed.position.set( 1000,1000,1000 );
+scene.add( directionalLightRed );
+// Green
+var directionalLightGreen = new THREE.DirectionalLight( 0x00ff00, 0.5 );
+directionalLightGreen.position.set( 1000,-1000,1000 );
+scene.add( directionalLightGreen );
+// Green
+var directionalLightBlue = new THREE.DirectionalLight( 0x0000ff, 0.5 );
+directionalLightBlue.position.set( -1000,-1000,1000 );
+scene.add( directionalLightBlue );
 
 camera.position.z = 5;
 
